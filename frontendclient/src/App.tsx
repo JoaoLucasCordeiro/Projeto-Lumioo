8ab06217e96eed564c1600bc02892b3d88a12551
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-13 20:39:05 -0300

    feat: novas rotas

--- frontendclient/src/App.tsx
+++ frontendclient/src/App.tsx
@@ -2,8 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/Login';
 import SignupPage from './pages/Signup';
-import { FeedPage } from './components/shared/Feed';
 import { PostDetails } from './components/shared/PostDetails';
+import { AllPosts } from './pages/AllPosts';
+import { FeedPage } from './pages/Feed';
+import { ProjectsPage } from './pages/Projects';
+import { ProfilePage } from './pages/Profile';
+import { SettingsPage } from './pages/SettingsPage';
+import { WorksPage } from './components/shared/Works';
 
 const App = () => {
   return (
@@ -12,11 +17,18 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/cadastro" element={<SignupPage />} />
+        <Route path="/perfil" element={<ProfilePage />} />
         <Route path="/feed" element={<FeedPage />} />
+        <Route path="/configuracoes" element={<SettingsPage />} />
+        <Route path="/trabalhos" element={<WorksPage />} />
+        <Route path="/posts" element={<AllPosts />} />
+        <Route path="/projetos" element={<ProjectsPage />} />
         <Route path="/post/:id" element={<PostDetails />} />
       </Routes>
     </BrowserRouter>
   );
+
+  
 }
 
 export default App;
