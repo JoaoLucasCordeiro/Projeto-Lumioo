f3a108e843cf6c62b912e66f32d49188667ef14a
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-19 18:56:35 -0300

    feat: rotas de projetos

---
+++ backendserver/src/routes/project.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from 'express';
+import { createProject, getAllProjects, getProjectById, updateProject, deleteProject } from '../controllers/project.controller';
+import { authenticateToken } from '../middlewares/auth.middleware';
+
+const router = Router();
+
+
+// Rotas públicas
+router.get('/projects', getAllProjects);
+router.get('/projects/:id', getProjectById);
+
+// Rotas protegidas
+router.post('/projects', authenticateToken, createProject);
+router.put('/projects/:id', authenticateToken, updateProject);
+router.delete('/projects/:id', authenticateToken, deleteProject);
+
+export default router;
