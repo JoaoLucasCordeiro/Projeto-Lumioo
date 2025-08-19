938c208282da78363b4e434fcf9c7eeb8fae08c3
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-19 17:26:06 -0300

    refactor: novas rotas adicionadas ao server.ts

--- backendserver/src/server.ts
+++ backendserver/src/server.ts
@@ -2,24 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes';
+import authRoutes from './routes/auth.routes';
+import postRoutes from './routes/post.routes'; 
 
 dotenv.config();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-// API Welcome Route
 app.get('/', (req, res) => {
   res.send('Lumioo API rodando com sucesso 🚀');
 });
 
-// Main API Routes v1
-app.use('/api/v1/lumioo', userRoutes); 
+app.use('/api/v1/lumioo', userRoutes);
+app.use('/api/v1/lumioo/auth', authRoutes);
+app.use('/api/v1/lumioo', postRoutes);
 
-// Server Initialization
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
