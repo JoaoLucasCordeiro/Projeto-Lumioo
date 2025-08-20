89b1db0edbffd4460bc4228b9cdafe263b10baab
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-20 12:26:45 -0300

    refactor: nova rota para editar comentário

--- backendserver/src/routes/comment.routes.ts
+++ backendserver/src/routes/comment.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { authenticateToken } from '../middlewares/auth.middleware';
-import { createComment, deleteComment, getCommentsForPost } from '../controllers/comments.controller';
+import { createComment, deleteComment, getCommentsForPost, updateComment } from '../controllers/comments.controller';
 
 const router = Router();
 
 router.post('/posts/:postId/comments', authenticateToken, createComment);
 router.get('/posts/:postId/comments', getCommentsForPost);
+router.put('/comments/:commentId', authenticateToken, updateComment);
 router.delete('/comments/:commentId', authenticateToken, deleteComment);
 
 export default router;
