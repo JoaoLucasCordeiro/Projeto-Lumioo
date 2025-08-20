b97baf6788ea6718e1396fe885aa8502ccebf3b8
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-20 12:33:00 -0300

    refactor: path de FeedLayout

--- frontendclient/src/pages/NewProject.tsx
+++ frontendclient/src/pages/NewProject.tsx
@@ -7,7 +7,7 @@ import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { FeedLayout } from "@/components/shared/FeedLayout";
+import { FeedLayout } from "@/components/shared/feed/FeedLayout";
 
 type TeamMember = {
   id: string;
