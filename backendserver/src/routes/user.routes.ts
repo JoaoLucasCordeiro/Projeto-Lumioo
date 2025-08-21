bf589bcdcedef599d431439a9801eab00607f851
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-21 19:07:57 -0300

    refactor: rota de atualizar usuário

--- backendserver/src/routes/user.routes.ts
+++ backendserver/src/routes/user.routes.ts
@@ -1,16 +1,18 @@
+// src/routes/user.routes.ts
 import { Router } from 'express';
-import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from '../controllers/user.controller';
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser, getMyProfile } from '../controllers/user.controller';
+import { authenticateToken } from '../middlewares/auth.middleware';
 
 const router = Router();
 
+// Agrupa as rotas do perfil do usuário logado
+router.get('/profile', authenticateToken, getMyProfile);
+router.put('/profile', authenticateToken, updateUser); // <-- ROTA CORRIGIDA AQUI
+
+// Rotas públicas ou para admin
 router.post('/users', createUser);
-
 router.get('/users', getAllUsers);
-
 router.get('/users/:id', getUserById);
-
-router.put('/users/:id', updateUser);
-
-router.delete('/users/:id', deleteUser);
+router.delete('/users/:id', authenticateToken, deleteUser); // Mantém para admin no futuro
 
 export default router;
