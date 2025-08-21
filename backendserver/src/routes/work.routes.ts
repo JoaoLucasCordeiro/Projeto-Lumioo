d42d0852405effa12ea93fd9a8616a1e4d64c568
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-21 13:43:48 -0300

    refactor: nova rota de download de trabalhos

--- backendserver/src/routes/work.routes.ts
+++ backendserver/src/routes/work.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
-import { createWork, getAllWorks, getWorkById, updateWork, deleteWork } from '../controllers/work.controller';
+import { createWork, getAllWorks, getWorkById, updateWork, deleteWork, downloadWorkById } from '../controllers/work.controller';
 import { authenticateToken } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 // Rotas públicas
+router.post('/works', authenticateToken, createWork);
 router.get('/works', getAllWorks);
 router.get('/works/:id', getWorkById);
-
-// Rotas protegidas
-router.post('/works', authenticateToken, createWork);
 router.put('/works/:id', authenticateToken, updateWork);
 router.delete('/works/:id', authenticateToken, deleteWork);
+
+// --- NOVA ROTA DE DOWNLOAD ---
+router.get('/works/:id/download', downloadWorkById);
 
 export default router;
