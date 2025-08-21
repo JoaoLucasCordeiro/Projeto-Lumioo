6f58afd5c11c1b685a05c32cd09cf953d1b30b69
JoaoLucasCordeiro <joaolucascttprofissional@gmail.com>
2025-08-21 20:10:57 -0300

    feat: rota de mudança de senha

--- backendserver/src/routes/user.routes.ts
+++ backendserver/src/routes/user.routes.ts
@@ -1,18 +1,20 @@
 // src/routes/user.routes.ts
 import { Router } from 'express';
-import { createUser, deleteUser, getAllUsers, getUserById, updateUser, getMyProfile } from '../controllers/user.controller';
+import { createUser, deleteUser, getAllUsers, getUserById, updateUser, getMyProfile, changePassword } from '../controllers/user.controller';
 import { authenticateToken } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 // Agrupa as rotas do perfil do usuário logado
 router.get('/profile', authenticateToken, getMyProfile);
-router.put('/profile', authenticateToken, updateUser); // <-- ROTA CORRIGIDA AQUI
+router.put('/profile', authenticateToken, updateUser);
 
 // Rotas públicas ou para admin
 router.post('/users', createUser);
 router.get('/users', getAllUsers);
 router.get('/users/:id', getUserById);
-router.delete('/users/:id', authenticateToken, deleteUser); // Mantém para admin no futuro
+router.delete('/users/:id', authenticateToken, deleteUser);
+router.put('/profile/password', authenticateToken, changePassword);
+
 
 export default router;
